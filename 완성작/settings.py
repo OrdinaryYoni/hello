5137e65869d9aa3b8cd97d5b3a0ddb7bed9a1565
YoNi <120238497+OrdinaryYoni@users.noreply.github.com>
2023-02-04 17:54:57 +0900

    @^-^@

---
+++ 완성작/settings.py
@@ -0,0 +1,29 @@
+#게임 설정
+TITLE = "Jumping On The Block!"
+GAME_WINDOW_WIDTH = 500
+GAME_WINDOW_HEIGHT = 800
+FPS = 30
+FONT_NAME = 'arial'
+
+#플레이어 설정
+PLAYER_ACC = 1.5
+PLAYER_FRICTION = -0.2
+PLAYER_GRAVITY = 0.8
+
+#블록 설정
+BLOCK_LIST = [(0, GAME_WINDOW_HEIGHT - 40, GAME_WINDOW_WIDTH, 40),
+              (480,150,80,80),
+              (350,GAME_WINDOW_HEIGHT / 2,80,80),
+              (GAME_WINDOW_WIDTH/2 - 50,GAME_WINDOW_HEIGHT*3/4,80,80),
+              (300,GAME_WINDOW_HEIGHT - 350,80,80),
+              (150,300,80,80)]
+
+#색
+LIGHTBLUE = (0, 155, 155)
+BGCOLOR = LIGHTBLUE
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+MAGENTA = (255, 0, 255)
