d7819ce64c0ee5acb6c011b22683629405d39c9a
YoNi <120238497+OrdinaryYoni@users.noreply.github.com>
2023-03-25 17:50:08 +0900

    Add files via upload

---
+++ 채팅프로그램/client.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+
+def Send(client_sock):
+    while True:
+        send_data = bytes(input().encode()) #사용자 입력
+        client_sock.send(send_data) #Client -> Server 데이터 송신
+
+def Recv(client_sock):
+    while True:
+        recv_data = client_sock.recv(1024).decode() #Server -> Client 데이터 송신
+        print(recv_data)
+
+#TCP Client
+if __name__ == '__main__':
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
+    Host = 'localhost' #통신할 대상의 IP 주소
+    Port = 9000 #통신할 대상의 Port 주소
+    client_sock.connect((Host, Port)) #서버로 연결시도
+    print('Connectin to', Host, Port)
+
+    #Client의 메세지를 보낼 쓰레드
+    thread1 = threading.Thread(target=Send, args=(client_sock,))
+    thread1.start()
+
+    #Server로 부터 다른 클라이언트의 메세지를 받을 쓰레드
+    thread2 = threading.Thread(target=Recv, args=(client_sock,))
+    thread2.start()
+
---
+++ 채팅프로그램/server.py
@@ -0,0 +1,60 @@
+import socket
+import threading
+from queue import Queue
+
+def Send(group, send_queue):
+    print('Thread Send Start')
+    while True:
+        try:
+            #새롭게 추가된 클라이언트가 있을 경우 Send 쓰레드를 새롭게 만들기 위해 루프를 빠져나감
+            recv = send_queue.get()
+            if recv == 'Group Changed':
+                print('Group Changed')
+                break
+
+            #for문을 돌면서 모든 클라이언트에게 동일한 메세지를 보냄
+            for conn in group:
+                msg = 'Client' + str(recv[2]) + '>>' + str(recv[0])
+                if recv[1] != conn: #client 본인이 보낸 메세지는 받을 필요가 없기 때문에 제외시킴
+                    conn.send(bytes(msg.encode()))
+                else:
+                    pass
+        except:
+            pass
+
+def Recv(conn,count,send_queue):
+    print('Thread Recv' + str(count) + 'Start')
+    while True:
+        data = conn.recv(1024).decode()
+        send_queue.put([data,conn,count]) #각각의 클라이언트의 메세지, 소켓정보, 쓰레드 번호를 send로 보냄
+
+#TCP Echo Server
+if __name__ == '__main__':
+    send_queue = Queue()
+    HOST = '' #수신 받을 모든 IP를 의미
+    PORT = 9000 #수신 받을 Port
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
+    server_sock.bind((HOST,PORT)) #소켓에 수신받을 IP주소와 PORT를 설정
+    server_sock.listen(10) #소켓 연결, 여기서 파라미터는 접속수를 의미
+    count = 0
+    group = [] #연결된 클라이언트의 소켓정보를 리스트로 묶기 위함
+    while True:
+        count = count + 1
+        conn,addr = server_sock.accept() #해당 소켓을 열고 대기
+        group.append(conn) #연결된 클라이언트의 소켓정보
+        print('Connected' + str(addr))
+
+        #소켓을 연결된 모든 클라이언트에게 동일한 메세지를 보내기 위한 쓰레드(브로드캐스트)
+        #연결된 클라이언트가 1명 이상일 경우 변경된 group 리스트로 반영
+
+        if count > 1:
+            send_queue.put('Group Changed')
+            thread1 = threading.Thread(target = Send, args = (group,send_queue,))
+            thread1.start()
+            pass
+        else:
+            thread1 = threading.Thread(target=Send,args=(group, send_queue,))
+            thread1.start()
+        #소켓에 연결된 각각의 클라이언트의 메세지를 받을 쓰레드
+        thread2 = threading.Thread(target=Recv,args=(conn,count,send_queue,))
+        thread2.start()
