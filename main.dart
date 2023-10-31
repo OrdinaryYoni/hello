db75e1f93387e9029b7e6769d2b3a4be1c1cfb5e
YoNi <120238497+OrdinaryYoni@users.noreply.github.com>
2023-11-01 02:00:23 +0900

    만들다가 졸려서 잘게유~~
    앱동아리 게시판 극 초반...

--- main.dart
+++ main.dart
@@ -1,57 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());    // 앱 실행
 
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '상세페이지',
-      home: MyHomePage(),
+      home: NoticeHeader(),
     );
   } 
 }
 
-class MyHomePage extends StatelessWidget {
+class NoticeHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('단팥빵'),
-        centerTitle: true,
-        backgroundColor: Colors.orange,
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 0, 0),
-        child: Column(
-          
-          children: <Widget> [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/단팥.jpg'),
-                radius: 200.0,
-              ),
-            ),
-            Divider(
-              height: 60,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text('단팥빵',
-            style: TextStyle(
-              letterSpacing: 2.0,
-              fontSize: 50,
-              fontWeight: FontWeight.bold
-            ),
-            )
-          ],
+        toolbarHeight: 100,
+        title: Text(
+          '게시판',
+          style: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        centerTitle: true,
+        backgroundColor: Color(0xff4EAAFF),
+        elevation: 0.0,
+        leading: Image.asset('image/ohsunggo.png',),
+        leadingWidth: 200,
       ),
     );
   }
